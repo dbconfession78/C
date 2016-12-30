acd6b5eb4ca8bddf43a1e2dc30b0989efed0dd16
johncoleman83 <johncoleman83@gmail.com>
2016-12-29 18:19:23 -0600

    djc ex 1-5

---
+++ djc_ex_1_5.c
@@ -0,0 +1,24 @@
+/* Program that prints table of farenheight to celsius conversion */
+#include <stdio.h>
+#define BOTTOM      -50  /* bottom of table (i.e. thermometer)*/
+#define TOP         300
+#define INCREMENT    10
+
+
+int main() {
+
+  float far, cel;
+  int bottom, top, increment;
+
+  far = BOTTOM;
+  printf("Farenheight and Cecius Conversion Table\n");
+  printf("Farenheight - Celsius\n");
+ 
+ while (far < TOP) {
+    cel = (5.0/9.0) * (far - 32.0);
+    printf("%8.0f  |  %6.1f\n", far, cel);
+  far += INCREMENT; }
+
+return 0;
+
+}
