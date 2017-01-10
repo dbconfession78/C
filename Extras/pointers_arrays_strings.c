7f410b90e62abe21aa6335bfe0bd6979366e3fbf
David John Coleman II <johncoleman83@gmail.com>
2017-01-10 08:03:59 -0800

    addresses = pointers = arrays = strings

--- Extras/pointers_arrays_strings.c
+++ Extras/pointers_arrays_strings.c
@@ -4,13 +4,15 @@ int main(void) {
 
 
   printf("\n\n\n-----------------------------------------------\n");
-  printf("Program to help with Arrays, Strings & Pointers\n");
+  printf("Program to show how addresses = pointers = arrays = strings\n");
   printf("-----------------------------------------------\n\n\n\n");
 
 
   int i;
   int integerA = 1;
-  int *pintegerA = &integerA;
+  int *pintegerA = &integerA;  /* pointer = address */
+  
+  /* output to console to show the equality of addresses and pointers */
   printf("testing integers, addresses, and pointers\n");
   printf("is this an integer--------------: %d\n", integerA);
   printf("is this the address of integerA-: %p\n", &integerA);
@@ -20,7 +22,8 @@ int main(void) {
 
     
   int array[] = {1, 2, 3, 4};
-    
+  
+  /* output to console to show the array */
   printf("testing array of integers\n");
   printf("is this an array------: { ");
   for (i=0; i < (sizeof (array) /sizeof (array[0])); i++) {
@@ -45,6 +48,7 @@ pointers_arrays_strings.c:32:8: warning: incompatible pointer types initializing
   **
   */
 
+  /* output to console to show equality of address, pointers and arrays */
   printf("testing address of array of integers\n");
   printf("addresses of indexes in an array----: { ");
   for (i=0; i < (sizeof (array) /sizeof (array[0])); i++) {
@@ -66,6 +70,7 @@ pointers_arrays_strings.c:32:8: warning: incompatible pointer types initializing
     
   char string_array[] = {'s', 't', 'r', 'i', 'n', 'g', '-', 'a', 'r', 'r', 'a', 'y', '\0'};
   
+  /* output to show equality of arrays and strings */
   printf("testing array of chars, non-conventional\n");
   printf("is this an array of chars: %s\n", string_array);
   printf("is this char @ index 0---: %c\n", string_array[0]);
@@ -75,6 +80,7 @@ pointers_arrays_strings.c:32:8: warning: incompatible pointer types initializing
   char *pstring = string;
   /* See above NOTE!! */
   
+  /* output to console to show addresses = pointers = arrays = strings */
   printf("testing address & pointers of string\n");
   printf("is this a string-------------------: %s\n", string);
   printf("is this address of string----------: %p\n", &string);
